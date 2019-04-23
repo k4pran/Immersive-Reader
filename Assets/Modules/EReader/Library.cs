07db8180a0410ac221262a1000b390e84ab4bbbc
Ryan McCauley <ryanmccauley211@gmail.com>
2019-04-23 09:02:46 +0100

    Add basic library structure with serialization / deserialization
    - Library holds shelves that can be used to organise library
    - Shelves can be named and hold books

---
+++ Assets/Modules/EReader/Library.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using YamlDotNet.Core;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace EReader {
+    
+    public class Library {
+
+        public List<Shelf> shelves { get; set; }
+        
+        public Library() {}
+
+        public Library(List<Shelf> shelves) {
+            this.shelves = shelves;
+        }
+
+        public void serialize() {
+            var serializer = new SerializerBuilder()
+                .WithTagMapping("!basicBook", typeof(BasicBook))
+                .WithTagMapping("!basicPage", typeof(BasicPage))
+                .EmitDefaults()
+                .DisableAliases()
+                .Build();            
+            var yaml = serializer.Serialize(this);
+            File.WriteAllText(Config.Instance.libraryPath, yaml);
+        }
+
+        public static Library Deserialize() {
+            
+            string libraryContent;
+            FileStream fileStream = new FileStream(Config.Instance.libraryPath, FileMode.Open, FileAccess.Read);
+            using (StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8)) {
+                libraryContent = streamReader.ReadToEnd();
+            }
+            
+            StringReader yamlInput = new StringReader(libraryContent);
+            Deserializer deserializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .WithTagMapping("!basicBook", typeof(BasicBook))
+                .WithTagMapping("!basicPage", typeof(BasicPage))
+                .Build();
+
+            Library library = deserializer.Deserialize<Library>(yamlInput);
+            return library;
+        }
+    }
+}
---
+++ Assets/Modules/EReader/Shelf.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace EReader {
+    
+    public class Shelf {
+        
+        public string shelfName { get; set; }
+        public List<Book> books { get; private set; }
+        
+        public Shelf() {}
+
+        public Shelf(List<Book> books, string shelfName) {
+            this.books = books;
+            this.shelfName = shelfName;
+        }
+    }
+}
